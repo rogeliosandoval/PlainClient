1d953877e5d7fc9cda5193dcf8571fd319ab6ba2
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-11-07 12:21:03 -0600

    begin team member logic

--- src/app/pages/dashboard/team-members/team-members.component.ts
+++ src/app/pages/dashboard/team-members/team-members.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core'
+import { AuthService } from '../../../services/auth.service'
+import { ButtonModule } from 'primeng/button'
+import { MemberFormDialog } from '../../../dialogs/member-form/member-form.component'
 
 @Component({
   selector: 'tc-team-members',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    MemberFormDialog
+  ],
   templateUrl: './team-members.component.html',
   styleUrl: './team-members.component.scss'
 })
-export class TeamMembers {
+
+export class TeamMembers implements OnInit {
+  @ViewChild('memberFormDialog') memberFormDialog!: MemberFormDialog
+  public authService = inject(AuthService)
+  public showMemberFormDialog = signal<boolean>(false)
+
+  ngOnInit(): void {
+
+  }
+
+  public onDialogClose(newState: boolean) {
+    this.showMemberFormDialog.set(newState)
+  }
 
 }
