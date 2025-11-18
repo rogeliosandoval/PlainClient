f877da8e0272e9c61f12ec055127e1f180ff877c
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-11-18 00:42:47 -0600

    refactor dark mode to save state for user/update todos

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, signal } from '@angular/core'
+import { Injectable, Inject, signal, PLATFORM_ID } from '@angular/core'
+import { isPlatformBrowser } from '@angular/common'
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,39 @@ export class SharedService {
   clientFormType = signal<string>('')
   dialogClient = signal<any>(null)
 
+  constructor(
+      @Inject(PLATFORM_ID)
+      private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      const storedTheme = localStorage.getItem('darkMode')
+      if (storedTheme !== null) {
+        this.darkMode.set(storedTheme === 'true')
+      }
+
+      this.applyTheme()
+    }
+  }
+
   toggleTheme(): void {
     this.darkMode.update(value => !value)
-    const themeLink = document.getElementById('theme-link') as HTMLLinkElement
-    if (themeLink) {
-      themeLink.href = this.darkMode()
-        ? 'assets/themes/lara-dark-blue/theme.css'
-        : 'assets/themes/lara-light-blue/theme.css'
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('darkMode', String(this.darkMode()))
+    }
+
+    this.applyTheme()
+  }
+
+  private applyTheme() {
+    if (isPlatformBrowser(this.platformId)) {
+      const themeLink = document.getElementById('theme-link') as HTMLLinkElement
+
+      if (themeLink) {
+        themeLink.href = this.darkMode()
+          ? 'assets/themes/lara-dark-blue/theme.css'
+          : 'assets/themes/lara-light-blue/theme.css'
+      }
     }
   }
 }
