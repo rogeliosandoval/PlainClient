78136a54bfea32af266cfc9c7099ce11579d8dd7
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-11-26 12:19:44 -0600

    begin profits page

--- src/app/pages/dashboard/profits/profits.component.ts
+++ src/app/pages/dashboard/profits/profits.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core'
+import { AuthService } from '../../../services/auth.service'
+import { ButtonModule } from 'primeng/button'
+import { MessageService } from 'primeng/api'
 
 @Component({
   selector: 'tc-profits',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule
+  ],
   templateUrl: './profits.component.html',
   styleUrl: './profits.component.scss'
 })
-export class Profits {
+export class Profits implements OnInit {
+  public authService = inject(AuthService)
+  public messageService = inject(MessageService)
 
+  ngOnInit(): void {
+
+  }
 }
