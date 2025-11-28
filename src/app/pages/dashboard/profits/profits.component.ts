cd5b4b9bbee2e006b0bb02ad09a02b827605e5ba
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-11-27 22:05:09 -0600

    implement tab for business and personal profits

--- src/app/pages/dashboard/profits/profits.component.ts
+++ src/app/pages/dashboard/profits/profits.component.ts
@@ -2,12 +2,15 @@ import { Component, inject, OnInit, signal, ViewChild } from '@angular/core'
 import { AuthService } from '../../../services/auth.service'
 import { ButtonModule } from 'primeng/button'
 import { MessageService } from 'primeng/api'
+import { TabMenuModule } from 'primeng/tabmenu'
+import { MenuItem } from 'primeng/api'
 
 @Component({
   selector: 'tc-profits',
   standalone: true,
   imports: [
-    ButtonModule
+    ButtonModule,
+    TabMenuModule
   ],
   templateUrl: './profits.component.html',
   styleUrl: './profits.component.scss'
@@ -15,8 +18,28 @@ import { MessageService } from 'primeng/api'
 export class Profits implements OnInit {
   public authService = inject(AuthService)
   public messageService = inject(MessageService)
+  public items: MenuItem[] | undefined
+  public activeItem: any
+  public showBusinessProfits = signal<boolean>(true)
+  public showPersonalProfits = signal<boolean>(false)
 
   ngOnInit(): void {
+    this.items = [
+      { 
+        label: 'Business',
+        icon: 'pi pi-briefcase',
+        command: () => {
+          
+        }
+      },
+      {
+        label: 'Personal',
+        icon: 'pi pi-user',
+        command: () => {
 
+        }
+      }
+    ]
+    this.activeItem = this.items[0]
   }
 }
