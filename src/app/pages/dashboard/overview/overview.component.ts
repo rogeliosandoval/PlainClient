6b19d826768cd7fee6839477fc74b5ae93e8d14f
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2026-01-05 11:28:03 -0600

    push progress for overview

--- src/app/pages/dashboard/overview/overview.component.ts
+++ src/app/pages/dashboard/overview/overview.component.ts
@@ -1,16 +1,41 @@
-import { Component, inject } from '@angular/core'
+import { Component, computed, inject, OnInit } from '@angular/core'
 import { SharedService } from '../../../services/shared.service'
+import { AuthService } from '../../../services/auth.service'
+import { CurrencyPipe } from '@angular/common'
 
 @Component({
   selector: 'tc-overview',
   standalone: true,
   imports: [
-
+    CurrencyPipe
   ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
 
-export class Overview {
+export class Overview implements OnInit {
   public sharedService = inject(SharedService)
+  public authService = inject(AuthService)
+
+  ngOnInit(): void {
+    
+  }
+
+  public ness(): void {
+    console.log(this.authService.coreBusinessData())
+  }
+
+  public parseAmount(amountStr: string): number {
+    const normalized = amountStr.replace(/,/g, '')
+    const num = parseFloat(normalized)
+    return isNaN(num) ? 0 : num
+  }
+
+  public totalBusinessProfit = computed(() => {
+    const profits = this.sharedService.businessProfits()
+    return profits.reduce((acc, p) => {
+      const amt = this.parseAmount(p.amount)
+      return p.profitType === 'Income' ? acc + amt : acc - amt
+    }, 0)
+  })
 }
