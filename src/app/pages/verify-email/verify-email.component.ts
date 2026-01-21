41c3a520567f6e0faa569dcd0a66287e525c1f7b
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2026-01-20 18:55:55 -0600

    finish email verification process/update UI

--- src/app/pages/verify-email/verify-email.component.ts
+++ src/app/pages/verify-email/verify-email.component.ts
@@ -1,15 +1,18 @@
-import { Component, inject, OnInit } from '@angular/core'
+import { Component, inject, OnInit, signal } from '@angular/core'
 import { ButtonModule } from 'primeng/button'
 import { PrimeNGConfig } from 'primeng/api'
 import { RouterLink, Router } from '@angular/router'
+import { SharedService } from '../../services/shared.service'
 import { Auth, sendEmailVerification, reload } from '@angular/fire/auth'
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'tc-verify-email',
   standalone: true,
   imports: [
     ButtonModule,
-    RouterLink
+    RouterLink,
+    NgOptimizedImage
   ],
   templateUrl: './verify-email.component.html',
   styleUrl: './verify-email.component.scss'
@@ -17,19 +20,37 @@ import { Auth, sendEmailVerification, reload } from '@angular/fire/auth'
 
 export class VerifyEmail implements OnInit {
   public primengConfig = inject(PrimeNGConfig)
+  public checking = signal<boolean>(false)
+  public sendingEmail = signal<boolean>(false)
+  public message = signal<string>('')
   private auth = inject(Auth)
   private router = inject(Router)
+  public sharedService = inject(SharedService)
 
   ngOnInit(): void {
     this.primengConfig.ripple = true
   }
 
   public async resendEmail(): Promise<void> {
-    await sendEmailVerification(this.auth.currentUser!)
+    this.sendingEmail.set(true)
+    await sendEmailVerification(this.auth.currentUser!).then(() => {
+      this.sendingEmail.set(false)
+      this.message.set('Email has been sent!')
+    }).catch((err) => {
+      console.log(err)
+      this.sendingEmail.set(false)
+      this.message.set('There was an error. Try again.')
+    })
   }
 
   public async continue(): Promise<void> {
+    this.checking.set(true)
     await reload(this.auth.currentUser!)
-    if (this.auth.currentUser?.emailVerified) this.router.navigateByUrl('/dashboard')
+    if (this.auth.currentUser?.emailVerified) {
+      this.router.navigateByUrl('/dashboard')
+    } else {
+      this.checking.set(false)
+      this.message.set('You are not verified yet. Try again.')
+    }
   }
 }
